6747b7a620e9db3a18c79732429a17d1a383c900
ACorrot <a.corrot@hotmail.fr>
2018-02-14 16:05:46 +0100

    Différents ajouts

--- sloubi.py
+++ sloubi.py
@@ -111,7 +111,17 @@ while pm >= 1:
             dadv = randrange(7)
             rep = "caca"
             time.sleep(1.5)
-            print(adv + " a sorti un %s," %(dadv) + " et vous regarde avec un air biaiseux.")
+            humeuradv = {0: "avec tellement d'intensité que ses veines oculaires sont plus grosses que son globe.",
+                         1: "avec un regard biaiseux.",
+                         2: "en explosant de rire, tout en vous postillonnant dessus.",
+            }
+            if dj > dadv:
+                test = 0
+            elif dj == dadv:
+                test = 1
+            elif dj < dadv:
+                test = 2
+            print(adv + " a sorti un %s," %(dadv) + " et vous regarde " + humeuradv[test])
             time.sleep(1.5)
 
             #Si le joueur gagne
@@ -132,15 +142,18 @@ while pm >= 1:
                         while papa != "a" and papa != "b" and papa != "c" and papa != "d" and papa != "e" and papa != "f":
                             papa = input("Vous avez plus que 5 solutions possibles. Laquelle choisissez-vous ? " + "\n" + "a) Compter" + "\n" + "b) Diviser par 6 le total des dés" + "\n" + "c) Annuler le tour" + "\n" + "d) Attrape oiseau" + "\n" + "e) Faire 20 jets de dés" + "\n" + "f) Chante Sloubi (conseillé)" + "\n")
                             if papa == "a":
+                                #Calcul des points
                                 print("Calcul des points...")
                                 time.sleep(1.5)
                                 print("Vous avez %s points grâce à votre jet de dé." %(dj))
                             elif papa == "b":
+                                #On divise tous les jets de dés par 6
                                 print("Division par 6 du total des dés...")
                                 ddj = dj / 6
                                 ddadv = dadv / 6
                                 print("Votre nouveau total de point est de %s points " %(ddj) + ", et celui de votre adversaire est de %s" %(dadv) + " points")
                             elif papa == "c":
+                                #Annulation du tour
                                 print("Vous avez annulé le tour, le total de vos points et ceux de votre adversaire tombent à 0 !")
                                 dj = 0
                                 dadv = 0
@@ -198,7 +211,10 @@ while pm >= 1:
                                     print("Sloubi " + str(sloubi)) 
                                     time.sleep(1)
                                 else:
-                                    print("Sloubi fini ?")
+                                    print("STOP ?")
+                                    time.sleep(1)
+                                    print("Non, là il faut dire 'Sloubi'............ Voilà !")
+                                    time.sleep(1)
                             else:
                                 "Merci d'entrer une vraie valeur"
                     elif rep == "b":
@@ -218,6 +234,87 @@ while pm >= 1:
                         print("Okay !")
                         time.sleep(0.4)
                         print("Tavernier, une mousse pour ce brave personnage, on continue la partie !")
+                        papa = "z"
+                        papa = "z"
+                        while papa != "a" and papa != "b" and papa != "c" and papa != "d" and papa != "e" and papa != "f":
+                            papa = input("Vous avez plus que 5 solutions possibles. Laquelle choisissez-vous ? " + "\n" + "a) Compter" + "\n" + "b) Diviser par 6 le total des dés" + "\n" + "c) Annuler le tour" + "\n" + "d) Attrape oiseau" + "\n" + "e) Faire 20 jets de dés" + "\n" + "f) Chante Sloubi (conseillé)" + "\n")
+                            if papa == "a":
+                                #Calcul des points
+                                print("Calcul des points...")
+                                time.sleep(1.5)
+                                print("Vous avez %s points grâce à votre jet de dé." %(dj))
+                            elif papa == "b":
+                                #On divise tous les jets de dés par 6
+                                print("Division par 6 du total des dés...")
+                                ddj = dj / 6
+                                ddadv = dadv / 6
+                                print("Votre nouveau total de point est de %s points " %(ddj) + ", et celui de votre adversaire est de %s" %(dadv) + " points")
+                            elif papa == "c":
+                                #Annulation du tour
+                                print("Vous avez annulé le tour, le total de vos points et ceux de votre adversaire tombent à 0 !")
+                                dj = 0
+                                dadv = 0
+                            elif papa == "d":
+                                #Attrape oiseau
+                                print("Vous avez fait le choix de l'attrape oiseau")
+                                time.sleep(2)
+                                os.system('clear')
+                                print("Je vais vous poser une question. Vous répondez par Oui, Non, ou Zbradaraldjan.") 
+                                time.sleep(2)
+                                print("Ok c'est parti :")
+                                time.sleep(3)
+                                attois = "sloubi"
+                                #Question de l'attrape oiseau
+                                while attois != "oui" and attois != "Oui" and attois != "Non" and attois != "non" and attois != "Zbradaraldjan" and attois != "zbradaraldjan" and attois != "Sur une branche" and attois != "sur une branche":    
+                                    attois = input("Où se trouve l'oiseau ?")
+                                    if attois == "Oui" or attois == "oui":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Non" or attois == "non":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Zbradaraldjan" or attois == "zbradaraldjan":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Sur une branche" or attois == "sur une branche":
+                                        print("Bonne réponse, on voit que vous avez l'oeil, que vous êtes un connaisseur !")
+                                        time.sleep(0.5)
+                                        print("*Clap Clap Clap*")
+                            elif papa == "e":
+                                print("Et c'est parti, on va faire 20 jets de 2 dés moins 4, ça fera un résultat compris entre 16 et 116..." )
+                                time.sleep(4)
+                                for i in range(1, 8):
+                                    print("*roll roll roll roll*")
+                                    time.sleep(0.4)
+                                sjdd = randrange(16, 116)
+                                print("Votre nouveau score s'élève donc maintenant à... %s points !" %(sjdd))
+
+                            elif papa == "f":
+                                print("Donc maintenant, on va faire le chante sloubi.")
+                                time.sleep(1)
+                                print("Vous comptez avec sloubi, et je vous arrête quand je tombe sur votre score. Vous êtes prêt ? ")
+                                time.sleep(1.5)
+                                print("Attention, loupez pas le coche !")
+                                time.sleep(2)
+                                sloubi = 0
+                                while sloubi < dj:
+                                    sloubi = sloubi + 1
+                                    print("Sloubi " + str(sloubi)) 
+                                    time.sleep(1)
+                                else:
+                                    print("STOP ?")
+                                    time.sleep(1)
+                                    print("Non, là il faut dire 'Sloubi'............ Voilà !")
+                                    time.sleep(1)
+                            else:
+                                "Merci d'entrer une vraie valeur"
+ 
                     elif rep == "b":
                         print("Parfait, allez plutôt nettoyer les cuvettes de la taverne et laissez les vrais jouer !")
                         sys.exit()
@@ -233,6 +330,86 @@ while pm >= 1:
                         print("Okay !")
                         time.sleep(0.4)
                         print("Tavernier, une mousse pour ce brave personnage, on continue la partie !")
+                        papa = "z"
+                        while papa != "a" and papa != "b" and papa != "c" and papa != "d" and papa != "e" and papa != "f":
+                            papa = input("Vous avez plus que 5 solutions possibles. Laquelle choisissez-vous ? " + "\n" + "a) Compter" + "\n" + "b) Diviser par 6 le total des dés" + "\n" + "c) Annuler le tour" + "\n" + "d) Attrape oiseau" + "\n" + "e) Faire 20 jets de dés" + "\n" + "f) Chante Sloubi (conseillé)" + "\n")
+                            if papa == "a":
+                                #Calcul des points
+                                print("Calcul des points...")
+                                time.sleep(1.5)
+                                print("Vous avez %s points grâce à votre jet de dé." %(dj))
+                            elif papa == "b":
+                                #On divise tous les jets de dés par 6
+                                print("Division par 6 du total des dés...")
+                                ddj = dj / 6
+                                ddadv = dadv / 6
+                                print("Votre nouveau total de point est de %s points " %(ddj) + ", et celui de votre adversaire est de %s" %(dadv) + " points")
+                            elif papa == "c":
+                                #Annulation du tour
+                                print("Vous avez annulé le tour, le total de vos points et ceux de votre adversaire tombent à 0 !")
+                                dj = 0
+                                dadv = 0
+                            elif papa == "d":
+                                #Attrape oiseau
+                                print("Vous avez fait le choix de l'attrape oiseau")
+                                time.sleep(2)
+                                os.system('clear')
+                                print("Je vais vous poser une question. Vous répondez par Oui, Non, ou Zbradaraldjan.") 
+                                time.sleep(2)
+                                print("Ok c'est parti :")
+                                time.sleep(3)
+                                attois = "sloubi"
+                                #Question de l'attrape oiseau
+                                while attois != "oui" and attois != "Oui" and attois != "Non" and attois != "non" and attois != "Zbradaraldjan" and attois != "zbradaraldjan" and attois != "Sur une branche" and attois != "sur une branche":    
+                                    attois = input("Où se trouve l'oiseau ?")
+                                    if attois == "Oui" or attois == "oui":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Non" or attois == "non":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Zbradaraldjan" or attois == "zbradaraldjan":
+                                        print("Désolé, mauvaise réponse.")
+                                        time.sleep(1)
+                                        print("La bonne réponse était : 'Sur une branche' (et oui, il y avait un piège !)")
+                                        time.sleep(3)
+                                    elif attois == "Sur une branche" or attois == "sur une branche":
+                                        print("Bonne réponse, on voit que vous avez l'oeil, que vous êtes un connaisseur !")
+                                        time.sleep(0.5)
+                                        print("*Clap Clap Clap*")
+                            elif papa == "e":
+                                print("Et c'est parti, on va faire 20 jets de 2 dés moins 4, ça fera un résultat compris entre 16 et 116..." )
+                                time.sleep(4)
+                                for i in range(1, 8):
+                                    print("*roll roll roll roll*")
+                                    time.sleep(0.4)
+                                sjdd = randrange(16, 116)
+                                print("Votre nouveau score s'élève donc maintenant à... %s points !" %(sjdd))
+
+                            elif papa == "f":
+                                print("Donc maintenant, on va faire le chante sloubi.")
+                                time.sleep(1)
+                                print("Vous comptez avec sloubi, et je vous arrête quand je tombe sur votre score. Vous êtes prêt ? ")
+                                time.sleep(1.5)
+                                print("Attention, loupez pas le coche !")
+                                time.sleep(2)
+                                sloubi = 0
+                                while sloubi < dj:
+                                    sloubi = sloubi + 1
+                                    print("Sloubi " + str(sloubi)) 
+                                    time.sleep(1)
+                                else:
+                                    print("STOP ?")
+                                    time.sleep(1)
+                                    print("Non, là il faut dire 'Sloubi'............ Voilà !")
+                                    time.sleep(1)
+                            else:
+                                "Merci d'entrer une vraie valeur"
+ 
                     elif rep == "b":
                         print("Parfait, allez plutôt nettoyer les cuvettes de la taverne et laissez les vrais jouer !")
                         sys.exit()
